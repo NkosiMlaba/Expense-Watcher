30d6e74f3a51f63a7c2d4e2032ec94eb42177ba2
Nkosikhona Mlaba <nmlaba023@student.wethinkcode.co.za>
2024-11-04 10:20:52 +0200

    removed redundant comments

--- src/main/java/weshare/server/Routes.java
+++ src/main/java/weshare/server/Routes.java
@@ -9,13 +9,9 @@ public class Routes {
     public static final String LOGIN_PAGE = "/";
     public static final String LOGIN_ACTION = "/login.action";
     public static final String LOGOUT = "/logout";
-
-    // Route constants for expenses
     public static final String EXPENSES = "/expenses";
     public static final String NEWEXPENSES = "/newexpense";
     public static final String EXPENSE_ACTION = "/expense.action";
-
-    // Route constants for payment requests
     public static final String PAYMENT_REQUEST = "/paymentrequest";
     public static final String PAYMENT_REQUEST_ACTION = "/paymentrequest.action";
     public static final String PAYMENT_REQUEST_SENT = "/paymentrequests_sent";
@@ -25,16 +21,11 @@ public class Routes {
 
     public static void configure(WeShareServer server) {
         server.routes(() -> {
-            // User authentication routes
             post(LOGIN_ACTION,  PersonController.login);
             get(LOGOUT,         PersonController.logout);
-
-            // Expense management routes
             get(EXPENSES,           ExpensesController.view);
             get(NEWEXPENSES, ExpensesController.newExpense);
             post(EXPENSE_ACTION, ExpensesController.expenseAction);
-
-            // Payment request routes
             get(PAYMENT_REQUEST, ExpensesController.payment_request);
             post(PAYMENT_REQUEST_ACTION, ExpensesController.payment_request_Action);
             get(PAYMENT_REQUEST_SENT, ExpensesController.payment_request_sent);
